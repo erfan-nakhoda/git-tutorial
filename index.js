99d60e20cb2c98c186179e691f1ccf7eca6130bf
Erfan Nakhoda <erfinakhoda@gmail.com>
2024-09-23 17:55:12 +0330

    adding index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 
-function findOrders() {
+function findOrders(req,res,next) {
+    console.log("order");
+    next();
     
 }
-function fillUser() {
-    
+function fillUser(req,res,next) {
+    console.log("user");
+    next()
 }
-function validateToken() {
-    
+function validateToken(req,res,next) {
+    console.log("Token");
+    next()
 }
-app.get('/', )
+app.get('/' ,findOrders,fillUser, validateToken);
+app.listen(3000, (err) => {
+    if (err) console.log(err);
+    console.log("http://localhost:3000");
+    
+    
+})
